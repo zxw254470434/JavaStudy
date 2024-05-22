552a345b22f366a6c47f1d8631a79359abc907b7
zuiaierhu <1209717572@qq.com>
2024-05-22 20:33:08 +0800

    更新了一些算法题

--- LeetCode/ListNode.java
+++ LeetCode/ListNode.java
@@ -2,14 +2,14 @@ public class ListNode {
     int val;
     ListNode next;
 
-    public ListNode() {
+    ListNode() {
     }
 
-    public ListNode(int val) {
+    ListNode(int val) {
         this.val = val;
     }
 
-    public ListNode(int val, ListNode next) {
+    ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
