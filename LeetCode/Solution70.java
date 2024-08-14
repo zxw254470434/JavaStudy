f3fee7a0d71936b22d735fd3462a363283b0528d
zhangxingwei_i <zhangxingwei_i@didiglobal.com>
2024-08-14 16:11:22 +0800

    动态规划：爬楼梯

--- LeetCode/Solution70.java
+++ LeetCode/Solution70.java
@@ -1,10 +1,10 @@
 public class Solution70 {
     public int climbStairs(int n) {
-        if (n <= 1) return 1;
-        int i_2 = 1, i_1 = 1;
+        if (n <= 2) return n;
+        int i_2 = 1, i_1 = 2;
         int curr = 0;
-        for (int i = 2; i <= n; i++) {
-            curr = i_2 + i_1;
+        for (int i = 3; i <= n; i++) {
+            curr = i_1 + i_2;
             i_2 = i_1;
             i_1 = curr;
         }
